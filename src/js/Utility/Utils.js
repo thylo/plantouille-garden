bb31599c2a46529c0a5896feceaad203b63fd998
Julien Moreau <thylogal@gmail.com>
2020-06-15 09:27:10 +0200

    Update Utils.js
    Je te conseilles vivement lodash ou ramda pour ne pas réecrire ce genre de petits helpers à chaque fois https://lodash.com/docs/ https://ramdajs.com/docs/

--- src/js/Utility/Utils.js
+++ src/js/Utility/Utils.js
@@ -1,4 +1,4 @@
-﻿//Set the first letter of a sring in upper case
+//Set the first letter of a sring in upper case
 export function capitalize(s) {
     if (typeof s !== 'string') return ''
     return s.charAt(0).toUpperCase() + s.slice(1)
